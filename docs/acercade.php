2bfb7ddc87cb5ae2f0efeaede2641c896dbbfc55
Wilmer <wilmer.jem@gmail.com>
2024-06-02 12:27:24 -0400

    guardando cambios

---
+++ docs/acercade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+echo "Este es un texto escrito en php";
+    ?>
+</body>
+</html>
